98f008fee5728d109adf9a4be640433df1cdd93f
2003salar <salar.mansoori.2003@gmail.com>
2024-05-04 21:57:59 +0200

    Get a specific comment in a project

--- routers/comments.js
+++ routers/comments.js
@@ -4,6 +4,37 @@ const isUserAuthenticated = require('./isUserAuthenticated');
 const { Users, Posts, Comments } = require('../models');
  
 // Get a specific comment in a post
+router.get('/:postId/:commentId', isUserAuthenticated, async (req, res) => {
+    try {
+        const { postId, commentId } = req.params;
+        if (!postId || isNaN(postId)) {
+            return res.status(400).json({success: false, message: 'Invalid post'});
+        }
+        if (!commentId || isNaN(commentId)) {
+            return res.status(400).json({success: false, message: 'Invalid comment id'});
+        }
+        const comment = await Comments.findOne({
+            where: {
+                id: commentId,
+                post_id: postId,
+            }, 
+                include: [
+                    {
+                        model: Users,
+                        as: 'user',
+                        attributes: ['id', 'username'],
+                    }
+                ],
+            
+        });
+        if (!comment) {
+            return res.status(404).json({success: false, message: 'Comment not found'});
+        } 
+        res.status(200).json({success: true, data: comment})
+    } catch (error) {
+        
+    }
+})
 // Comment in a post
 router.post('/:id', isUserAuthenticated, async (req, res) => {
     try {
@@ -43,7 +74,8 @@ router.post('/:id', isUserAuthenticated, async (req, res) => {
     
 });
 // Reply to a comment
-// Edit a comment if the user is the commentor 
+// Edit a comment if the user is the commentor
+
 // Delete a comment if the users is the commentor
 
 module.exports = router;
